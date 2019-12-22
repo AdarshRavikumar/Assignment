aa1a42f3d907c1a8312bd1b5e28da63123d0d648
Adarsh_Ravikumar <40430208+AdarshRavikumar@users.noreply.github.com>
2019-12-22 19:55:37 +0530

    Update homeTest.java

--- question1/src/test/homeTest.java
+++ question1/src/test/homeTest.java
@@ -1,10 +1,7 @@
 package test;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
 import question1.*;
-
 
 class homeTest {
 
@@ -16,7 +13,6 @@ class homeTest {
 		home homeobject = new home();
 		item1 = homeobject.Assign(item, args, 0);
 		
-		
 		assertNotNull(item1,"The Object is created with Null Value");
 		assertNotNull(item1.getName(),"Name is Required");
 		assertNotNull(item1.getType(),"Type is Required");
@@ -26,6 +22,4 @@ class homeTest {
 		assertEquals(item1.getPrice(),13456);
 		assertEquals(item1.getType(),"raw","The Type don't match");
 	}
-	
-
 }
